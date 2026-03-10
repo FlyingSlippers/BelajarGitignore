34cec5ab0b560268f16c20c8a4ed476befd4d90e
FlyingSlippers <erebuscontacts@gmail.com>
2026-03-10 10:33:14 +0700

    nambahin script untuk nama

---
+++ Assets/Scripts/NamaSaya.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class NamaSaya : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Nama saya Kenzie");
+    }
+
+    
+}
